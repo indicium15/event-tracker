e1ecd94860872510cd414c6caa87e5cbef00c023
indicium15 <67154087+indicium15@users.noreply.github.com>
2025-11-14 23:42:31 +0800

    Add favicon, robots.txt and sitemap.xml

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, Response, request
+from datetime import datetime
 from football.routes import bp as football_bp
 from floorball.routes import bp as floorball_bp
 from tennis.routes import bp as tennis_bp
@@ -34,6 +35,48 @@ def create_app():
     @app.route("/")
     def home():
         return render_template("home.html")
+
+    @app.route("/robots.txt")
+    def robots_txt():
+        """Serve robots.txt file"""
+        robots_content = """
+            User-agent: *
+            Allow: /
+            Disallow: /static/
+        """
+        return Response(robots_content, mimetype='text/plain')
+
+    @app.route("/sitemap.xml")
+    def sitemap_xml():
+        """Generate sitemap.xml with all routes"""
+        base_url = request.url_root.rstrip('/')
+        
+        # Get current date in W3C format
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        
+        # Define all routes with their priorities and change frequencies
+        routes = [
+            {'loc': base_url, 'priority': '1.0', 'changefreq': 'daily'},
+            {'loc': f'{base_url}/football', 'priority': '0.8', 'changefreq': 'weekly'},
+            {'loc': f'{base_url}/floorball', 'priority': '0.8', 'changefreq': 'weekly'},
+            {'loc': f'{base_url}/tennis', 'priority': '0.8', 'changefreq': 'weekly'},
+            {'loc': f'{base_url}/basketball', 'priority': '0.8', 'changefreq': 'weekly'},
+        ]
+        
+        # Generate XML sitemap
+        sitemap_xml_content = '<?xml version="1.0" encoding="UTF-8"?>\n'
+        sitemap_xml_content += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+        
+        for route in routes:
+            sitemap_xml_content += '  <url>\n'
+            sitemap_xml_content += f'    <loc>{route["loc"]}</loc>\n'
+            sitemap_xml_content += f'    <lastmod>{current_date}</lastmod>\n'
+            sitemap_xml_content += f'    <changefreq>{route["changefreq"]}</changefreq>\n'
+            sitemap_xml_content += f'    <priority>{route["priority"]}</priority>\n'
+            sitemap_xml_content += '  </url>\n'
+        
+        sitemap_xml_content += '</urlset>'
+        return Response(sitemap_xml_content, mimetype='application/xml')
 
     return app
 
